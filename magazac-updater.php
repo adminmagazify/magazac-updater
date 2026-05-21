cdb1114d392b55800aef19cd3b7627a5019b5863
emirhan çağman <admin@magazify.com>
2026-05-21 12:06:12 +0300

    version 2.0

--- magazac-updater.php
+++ magazac-updater.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Magazac Updater
 Description: Otomatik Site Güncelleme Eklentisi
-Version: 1.6
+Version: 2.0
 Author: Magazac
 * GitHub Plugin URI: https://github.com/adminmagazify/magazac-updater
 */
